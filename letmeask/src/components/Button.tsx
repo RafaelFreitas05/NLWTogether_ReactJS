3fa239e7cdb66f9d236c80c9c561971f91fec5c0
rafaelfreitas05 <rafael10.nascimento@hotmail.com>
2021-08-05 14:56:41 -0300

    Conexão Firebase e página Home

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,10 +1,16 @@
-//Propriedades do botão
-type ButtonProps = {
-    text?: string;
-}
+import { useState } from "react";
 
-export function Button(props: ButtonProps){
+export function Button(){
+
+    const [ counter, setCounter ] = useState(0);
+
+    function increment(){
+        setCounter(counter + 1);        
+    }
+
     return(
-        <button>{ props.text }</button>
+        <button onClick={increment}>
+            {counter}
+        </button>
     )
 }
