8199e1537ff16fd810381ee52809a09c3c461b98
rafaelfreitas05 <rafael10.nascimento@hotmail.com>
2021-08-06 12:20:25 -0300

    Página Home e NewRoom Finalizadas

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,9 +1,17 @@
-import { Button } from './components/Button'
+//Biblioteca para rotas
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+
 
 function App() {
   return (
-    <Home />
+    //Criação das rotas
+    <BrowserRouter>  
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,25 +1,11 @@
-import { useHistory } from 'react-router-dom'
-import { auth, firebase } from '../services/firebase'
-import { Link } from 'react-router-dom'
 import illustrationIMG from '../assets/images/illustration.svg';
 import logoIMG from '../assets/images/logo.svg';
 import googleIconIMG from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button'
 import '../styles/auth.scss';
+import { Link } from 'react-router-dom';
 
-export function Home(){
-
-    const history = useHistory();
-
-    function handleCreateRoom(){
-        const provider = new firebase.auth.GoogleAuthProvider
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result)
-        })
-
-        //history.push('/rooms/new')
-    }
-
+export function NewRoom(){
     return (
         <div id="page-auth">
             <aside>
@@ -30,22 +16,19 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoIMG} alt="Letmeask" />
-                    <button onClick={handleCreateRoom} className="create-room"> 
-                        <img src={ googleIconIMG } alt="Logo do Google" />
-                        Crie sua sala com o Google                        
-                    </button>
-                    <div className="separator">
-                        ou entre em uma sala
-                    </div>
+                    <h2>Cria uma nova sala</h2>
                     <form>
                         <input 
                             type="text" 
-                            placeholder="Digite o código da sala"                        
+                            placeholder="Nome da sala"                        
                         />
                         <Button type="submit">
-                            Entrar na sala
+                            Criar sala
                         </Button>
                     </form>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+                    </p>
                 </div>
             </main>
         </div>
