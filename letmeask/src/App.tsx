591478e9b459101ff0dc9256776c43337a2aca53
rafaelfreitas05 <rafael10.nascimento@hotmail.com>
2021-08-18 11:32:50 -0300

    Exibição das perguntas e like

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,11 +1,10 @@
 //Biblioteca para rotas
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 import { Room } from './pages/Room'
-
 import { AuthContextProvider } from './contexts/AuthContext'
+import { AdminRoom } from './pages/AdminRoom'
 
 
 
@@ -19,6 +18,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
         <Route path="/rooms/:id" component={Room} />
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch>
     </AuthContextProvider>
     </BrowserRouter>
